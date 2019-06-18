f0cf7caabbf29cd425813de437c3880c8d08e8e6
Andrew G <gandriyko@gmail.com>
2019-06-18 11:31:57 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
+
 setup(
     name='gutils',
     version='0.1',
     description='Python Utility Libraries for "GShop" web-engine',
     author='Andrew G',
     author_email='gandriyko@gmail.com',
-    url="https://github.com/gandriyko/gultis",
+    url="https://github.com/gandriyko/gutils",
     packages=['gutils'],
-    install_requires=[],
     include_package_data=True,
     zip_safe=False,
     license="MIT",
