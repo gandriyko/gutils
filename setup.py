1dba6f8518b9ff2fcf55fb1bd3b2f0dfcb85734f
Andrew G <gandriyko@gmail.com>
2019-06-18 10:25:28 +0300

    move files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='gutils',
+    version='0.1',
+    description='Python Utility Libraries for "GShop" web-engine',
+    author='Andrew G',
+    author_email='gandriyko@gmail.com',
+    url="https://github.com/gandriyko/gultis",
+    packages=['gutils'],
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+    license="MIT",
+)
