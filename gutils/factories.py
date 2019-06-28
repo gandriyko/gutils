21209e8fd23000736e2008eda57c2648d4445b32
Andrew G <gandriyko@gmail.com>
2019-06-28 15:02:18 +0300

    PhoneProvider

--- gutils/factories.py
+++ gutils/factories.py
@@ -1,4 +1,5 @@
 from factory import fuzzy, random
+from faker.providers import BaseProvider
 
 
 class PhoneFuzzy(fuzzy.BaseFuzzyAttribute):
@@ -8,3 +9,10 @@ class PhoneFuzzy(fuzzy.BaseFuzzyAttribute):
                     '38096', '38097', '38098', '38099')
         return '%s%07d' % (random.randgen.choice(prefixes), random.randgen.randrange(0, 9999999))
 
+
+class PhoneProvider(BaseProvider):
+    def random_phone(self):
+        prefixes = ('38050', '38063', '38066', '38067', '38068', '38073',
+                    '38091', '38092', '38093', '38094', '38095',
+                    '38096', '38097', '38098', '38099')
+        return '%s%07d' % (random.randgen.choice(prefixes), random.randgen.randrange(0, 9999999))
