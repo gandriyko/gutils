21819242b094e14dad05cff4942e5186dd9e18af
Andriy Koziy <andriy@koziy.com>
2022-09-11 02:51:57 +0300

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='gutils',
-    version='0.5',
+    version='0.6',
     description='Python Utility Libraries for "GShop" web-engine',
     author='Andrew G',
     author_email='gandriyko@gmail.com',
